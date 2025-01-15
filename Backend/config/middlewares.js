d32c9be747d7ee15771ce150bd3dfb7c834bd180
Abhii01 <avee.abhishek220@gmail.com>
2025-01-15 16:21:18 +0530

    server file code changes

--- Backend/config/middlewares.js
+++ Backend/config/middlewares.js
@@ -6,7 +6,10 @@ const setupMiddlewares = (app) => {
     app.use(bodyParser.json());
     app.use(cors({
         origin: ['http://localhost:5173', 'https://mobzway-task01.onrender.com'],
-    }))
+        methods: 'GET,POST,PUT,DELETE',
+        credentials: true,
+      }));
+      
     app.use(helmet());
 };
 
