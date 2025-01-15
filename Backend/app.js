6276fc83dd14486290338373637af1a960a113b9
Abhii01 <avee.abhishek220@gmail.com>
2025-01-15 16:37:07 +0530

    server file code changes

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5100;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "https://mobzway-task01.onrender.com",
+    origin: "http://localhost:5173" || "https://mobzway-task01.onrender.com",
     methods: ["GET", "POST"],
   },
 });
