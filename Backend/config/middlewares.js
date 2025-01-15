169afeb6217baa14554ca3ff15dc7867da5d8e92
Abhii01 <avee.abhishek220@gmail.com>
2025-01-15 16:52:50 +0530

    server file code changes

--- Backend/config/middlewares.js
+++ Backend/config/middlewares.js
@@ -2,14 +2,21 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 
+const allowedOrigins = ['https://nodetask01mobzway.netlify.app'];
+
 const setupMiddlewares = (app) => {
     app.use(bodyParser.json());
     app.use(cors({
-        origin: ['http://localhost:5173', 'https://mobzway-task01.onrender.com'],
-        methods: 'GET,POST,PUT,DELETE',
-        credentials: true,
-      }));
-      
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST'],
+    }));
+
     app.use(helmet());
 };
 
