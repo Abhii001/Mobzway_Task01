ae957366e2e972c2902729a7ca208db67a7faba7
Abhii01 <avee.abhishek220@gmail.com>
2025-01-15 14:52:22 +0530

    frontend some code changes

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -6,53 +6,39 @@ const Header = () => {
     <header className="bg-blue-600 text-white shadow-md p-4">
       <nav className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">
-          <Link to="/">My Tasks</Link>
+          <Link to="/">TASKS</Link>
         </div>
 
-        <div className="space-x-4">
-          <Link
-            to="/"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 01
-          </Link>
+        <div className="space-x-4 flex items-center">
           <Link
             to="/liveUsers"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
+            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200 flex items-center"
           >
-            Task 02
+            Live
+            <span className="ml-2 w-3 h-3 rounded-full animate-blink"></span>
           </Link>
-          <Link
-            to="/task03"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 03
-          </Link>
-          <Link
-            to="/task04"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 04
-          </Link>
-          <Link
-            to="/task05"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 05
-          </Link>
-          <Link
-            to="/task06"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 06
-          </Link>
-          <Link
-            to="/task07"
-            className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200"
-          >
-            Task 07
-          </Link>
+          <style>
+            {`
+                  .animate-blink {
+                    background-color: red;
+                    animation: blink 5s infinite alternate;
+                  }
+                  
+                  @keyframes blink {
+                    0% {
+                      background-color: red;
+                    }
+                    50% {
+                      background-color: green;
+                    }
+                    100% {
+                      background-color: red;
+                    }
+                  }
+                `}
+          </style>
         </div>
+
       </nav>
     </header>
   );
